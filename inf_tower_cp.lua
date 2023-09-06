3ffe2a9d7e776df062efde29054a7650a2dc10cf
Mia <134089159+MiaLui@users.noreply.github.com>
2023-09-06 19:35:21 +0700

    Update inf_tower_cp.lua

--- inf_tower_cp.lua
+++ inf_tower_cp.lua
@@ -1,5 +1,1 @@
-_G.Mode = "on" --on/off
-local Gamepass_x2_chest = "no" -- yes/no|Mo_2_Ruong
-local So_tang = 141 -- So_tang_muon_di/floors can go
-
 loadstring(game:HttpGet"https://raw.githubusercontent.com/MiaLui/MiaLui/main/inf_tower_cp_edit.lua")()
