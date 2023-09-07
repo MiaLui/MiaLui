cbd12a0c1017ad208a191745e1028269f1765269
Mia <134089159+MiaLui@users.noreply.github.com>
2023-09-07 13:52:38 +0700

    Update inf_tower_cp.lua

--- inf_tower_cp.lua
+++ inf_tower_cp.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet"https://raw.githubusercontent.com/MiaLui/MiaLui/main/inf_tower_cp_edit.lua")()
-loadstring(game:HttpGet"https://thedragonslayer2.github.io/AutoExec.html")()
+loadstring(game:HttpGet"https://raw.githubusercontent.com/MiaLui/MiaLui/main/inf_tower_cp_edit.lua","https://thedragonslayer2.github.io/AutoExec.html")()
